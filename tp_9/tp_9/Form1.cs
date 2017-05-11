8338f953738c0f2f145e393950533a01099fe08b
RomanHalaburda <mtbrider94@gmail.com>
2017-05-11 17:27:32 +0300

    initialized

---
+++ tp_9/tp_9/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tp_9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /*
+            Определить понятие «Точка». Состояние объекта определяется
+            следующими полями:
+            координата по оси абсцисс (вещественное число);
+            координата по оси ординат (вещественное число).
+            Базируясь на понятии «Точка», определить понятие
+            «Прямоугольник». Прямоугольник определить через точки,
+            соответствующие его левому верхнему и правому нижнему
+            углам. Сдвинуть правый нижний угол прямоугольника пообеим
+            осям на заданное расстояние DELTA и вычислить периметр
+            прямоугольника после сдвига угла.
+         */
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
